67d4b5c2ff6326619bdd26aad4fdaa7c6e5ce31f
joon <khjoon372@naver.com>
2023-09-27 01:14:51 +0900

    jpa1

---
+++ JPA1/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+//스프링이 제공하는 기본 annotation
+@Repository
+public class MemberRepository {
+
+    //스프링 부트가 Entity Manager를 생성하는 작업을 다 해준다.
+    @PersistenceContext
+    private EntityManager em;
+
+    //저장을 하고 나면 command성이기 때문에 return 값을 만들지 않는다.(id정도만 있으면 조회가능)
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();// command와 query를 분리시킨다.
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+
+
+}
---
+++ JPA1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop;
+
+import com.fasterxml.jackson.databind.deser.std.StdKeyDeserializer;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)//Junit에 알려줘야함
+@SpringBootTest//Springboot로 테스트를 돌려야함
+public class MemberRepositoryTest {
+
+
+    @Autowired MemberRepository memberRepository; //memberRepository test 해보기 위함
+
+    @Test
+    @Transactional//entity manager를 통한 모든 데이터 변경은 transaction 안에서 이뤄져야한다.
+    //spring framework를 사용하기 때문에 springframework 라이브러리 권장
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        //memberRepository.save(member); 코드에 Ctrl+Alt+V하면 Extract에서 변수 뽑아오는 코드로 바꿔준다.
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());//검증은 스프링에 assertj 라이브러리를 가지고 있기 때문에 Assertions 바로 사용가능
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+
+
+}
