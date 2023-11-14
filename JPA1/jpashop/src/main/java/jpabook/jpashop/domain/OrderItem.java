10059198582f9d6fc36edfa847e83759d27450a4
joon <khjoon372@naver.com>
2023-11-15 01:51:53 +0900

    주문,주문 상품 Entity

--- JPA1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ JPA1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,4 +25,24 @@ public class OrderItem {
 
     private int count;//주문 수량
 
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice,int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);//Item이 order될 때 재고를 줄여주는 로직
+        return orderItem;
+    }
+
+    //===비즈니스 로직==//
+    public void cancel(){
+        getItem().addStock(count);//재고수량 원상복구 시킴
+
+    }
+
+    public int getTotalPrice(){
+        return getOrderPrice() * count;
+    }
 }
