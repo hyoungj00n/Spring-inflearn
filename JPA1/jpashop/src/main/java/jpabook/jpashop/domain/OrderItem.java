40d61a190fc8ed35ada001168c50fbf41b0ebd05
joon <khjoon372@naver.com>
2023-11-17 22:24:10 +0900

    Order 서비스

--- JPA1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ JPA1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,12 +2,15 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)//로직에서 기본 생성자의 생성을 막기 위함
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -24,6 +27,7 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
 
     private int count;//주문 수량
+
 
     //==생성 메서드==//
     public static OrderItem createOrderItem(Item item, int orderPrice,int count){
